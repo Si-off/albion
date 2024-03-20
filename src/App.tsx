14df7d1451adcc0c26859c1e9fbbb7f1be3ad179
Si-off <noismik97@gmail.com>
2024-03-20 14:04:21 +0900

    feat: font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '../src/styles/global/normalize.css';
+import '../src/styles/global/global.css';
+import { Button } from '@mui/material';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div>gkasdjks</div>
+      <Button>hello world</Button>
+    </div>
+  );
 }
 
 export default App;
